6f87fd88974aee665847545d82773d1e188b0b51
mmgwebi <mihlalimgwebi@gmail.com>
2024-12-20 22:33:49 +0200

    feat: :lipstick: style join session page

--- src/app/manage-groups/page.tsx
+++ src/app/manage-groups/page.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useReceiptContext } from "../scan-bill/ReceiptContext";
 
-
 export default function JoinSessionPage() {
   const { socket, receipt, setReceipt } = useReceiptContext();
   const router = useRouter();
@@ -23,7 +22,7 @@ export default function JoinSessionPage() {
       });
     }
   }, [socket, setReceipt]);
-  
+
   function joinSession() {
     if (socket && receipt) {
       socket.emit("join_session", {
@@ -35,15 +34,26 @@ export default function JoinSessionPage() {
   }
 
   return (
-    <div>
-      <h1>Join a Session</h1>
+    <div className='flex flex-col items-center justify-center w-full h-full gap-4 px-16'>
+      <label
+        htmlFor='sessionId'
+        className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+        Your session id
+      </label>
       <input
-        type="text"
+        id='sessionId'
+        type='text'
         value={sessionId}
         onChange={(e) => setSessionId(e.target.value)}
-        placeholder="Enter session ID"
+        placeholder='Enter session ID'
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        required
       />
-      <button onClick={joinSession}>Join session</button>
+      <button
+        className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        onClick={joinSession}>
+        Join session
+      </button>
     </div>
   );
 }
