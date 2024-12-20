21ce72c844f95d79afd5afd1f90a98475af380ba
mmgwebi <mihlalimgwebi@gmail.com>
2024-12-20 17:08:18 +0200

    fix: build issues

---
+++ src/app/split-bill-confirmation/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplitBillConfirmationPage = () => {
+  return (
+    <div>
+      <h1>TODO SplitBillConfirmationPage</h1>
+    </div>
+  );
+};
+
+export default SplitBillConfirmationPage;
